6e2b3f5b650a6352dce6b1ce8f9a1982566ed4f9
AntLouiz <luizrodrigo46@hotmail.com>
2018-07-16 20:23:57 -0300

    model Game added

--- games/models.py
+++ games/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Game(models.Model):
-	"""docstring for Game"""
-	def __init__(self, arg):
-		super(Game, self).__init__()
-		self.arg = arg
-		
+	created = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=200, blank=True, default='')
+	release_date = models.DateTimeField()
+	game_category = models.CharField(max_length=200, blank=True, default='')
+	played = models.BooleanField(default=False)
+
+	class Meta:
+		ordering = ('name',)
